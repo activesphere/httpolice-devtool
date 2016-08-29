b988ff4df468c59f55d155c2529d288ca1ca0f25
Rohitt Shinde <shinde.rohitt@gmail.com>
2016-08-29 14:32:14 +0530

    change default remote address

--- js/defaults.js
+++ js/defaults.js
@@ -1,7 +1,7 @@
 
 
 // Default server from where to get requests analyzed from
-export const REMOTE = 'http://ec2-52-66-144-43.ap-south-1.compute.amazonaws.com:8080/har';
+export const REMOTE = 'http://ec2-52-66-139-106.ap-south-1.compute.amazonaws.com:8080/har';
 
 // Default chunk size in which the headers should be sent to analyze
 export const BITESIZE = 5;
